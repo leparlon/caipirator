7dc07b42efce10afec31c943c778070803194012
Paulo Candolo Nogueira <paulocn@MACCIT011232.local>
2015-06-10 09:59:37 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # caipirator
 Google extension to change some words to its caipira equivalent
+
+# Instalation
+-Clone the repo on your machine
+-Go to google extensions page
+-Click Load Unpacked Extension
+-DONE
---
+++ content_script.js
@@ -0,0 +1,63 @@
+walk(document.body);
+
+
+function walk(node) 
+{
+	//this is heavily based on the cloud to butt extension
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+String.prototype.capitalizeFirstLetter = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+function handleText(textNode) 
+{
+
+	var v = textNode.nodeValue;
+	v = v.replace(/\ba gente\b/g, "agente");
+	v = v.replace(/\bnós\b/g, "nois");
+	v = v.replace(/\bpra mim\b/g, "preu");
+	v = v.replace(/\bpra eu\b/g, "preu");
+	v = v.replace(/\bmenos\b/g, "menas");
+	v = v.replace(/\bnegócio\b/g, "trem");
+	v = v.replace(/!/g, ", sô!");
+	v = v.replace(/\bigual\b/g, "quiném");
+
+
+	v = v.replace(/\bA gente\b/g, "Agente");
+	v = v.replace(/\bNós\b/g, "Nois");
+	v = v.replace(/\bPra mim\b/g, "Preu");
+	v = v.replace(/\bPra eu\b/g, "Preu");
+	v = v.replace(/\bMenos\b/g, "Menas");
+	v = v.replace(/\bNegócio\b/g, "Trem");
+	v = v.replace(/!/g, ", sô!");
+	v = v.replace(/\bIgual\b/g, "Quiném");
+
+	
+	textNode.nodeValue = v;
+}
+
+
+
+
